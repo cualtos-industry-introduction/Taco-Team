6cdd3780e5a3553662df8bdd999725ab81bde5d7
Diego448 <diegosg448@gmail.com>
2020-07-06 17:15:55 -0500

    Modulo agenda actualizado con ultimos cambios

--- agenda.py
+++ agenda.py
@@ -1,13 +1,28 @@
 import json
+from contacto import Contacto
 
 class Agenda():
     def __init__(self, nombre):
+        """Crea un nuevo objeto agenda con el nombre indicado en
+        el argumento.
+        Argumentos:
+        nombre -> str
+        Valor de retorno:
+        () -> Agenda
+        """
         self.contactos = []
         self.nombre = nombre
         self.archivo = nombre + '.tdb'
         self.cargar()
 
     def cargar(self):
+        """Carga los datos de una agenda guardada anteriormente,
+        los datos son cargados de un documento de texto.
+        Argumentos:
+        (ninguno)
+        Valor de retorno:
+        (ninguno)
+        """
         try:
             with open(self.archivo, 'r') as db:
                 for registro in db:
@@ -15,7 +30,6 @@ class Agenda():
                     contacto = json.loads(registro)
                     self.contactos.append(contacto)
         except FileNotFoundError as error:
-            print(error)
             pass
 
     def guardar(self):
@@ -27,8 +41,19 @@ class Agenda():
     def agregarContacto(self, contacto):
         if isinstance(contacto, dict):
             self.contactos.append(contacto)
+        elif isinstance(contacto, Contacto):
+            self.contactos.append(contacto.obtenerDatos())
         else:
             raise TypeError
+
+    def agregarContactos(self, lista_contactos):
+        if isinstance(lista_contactos, list):
+            self.contactos.clear()
+            for contacto in lista_contactos:
+                if isinstance(contacto, Contacto):
+                    self.contactos.append(contacto.obtenerDatos())
+                else:
+                    raise TypeError
 
     def mostrarContactos(self):
         return self.contactos
@@ -39,3 +64,10 @@ class Agenda():
                 return contacto.copy()
             else:
                 return {}
+
+    def obtenerContactos(self):
+        lista_contactos = []
+        for contacto in self.contactos:
+            nuevo_contacto = Contacto(contacto)
+            lista_contactos.append(nuevo_contacto)
+        return lista_contactos
